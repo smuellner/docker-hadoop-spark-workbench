2d2765f0ddd8a32cf52f3c3d54cd29909b00f431
Sascha Müllner <sascha@muellner.de>
2018-08-16 19:44:02 +0200

    improved environment

--- automotive/automotive.py
+++ automotive/automotive.py
@@ -8,13 +8,13 @@ spark = SparkSession \
 
 # join the dataframes
 accelerations = spark.read.csv("hdfs://192.168.0.104/user/automotive/accelerations.csv", header=True, mode="DROPMALFORMED").rdd
-rdd = accelerations.map(lambda x: (x['trip_id'], x[1:])).groupByKey().mapValues(list).collect().rdd
+# rdd = accelerations.map(lambda x: (x['trip_id'], x[1:])).groupByKey().mapValues(list).collect().rdd
 
-# gyroscopes = spark.read.csv("hdfs://192.168.0.104/user/automotive/gyroscopes.csv", header=True, mode="DROPMALFORMED")
-# joined = accelerations.join(gyroscopes, on=['trip_id', 'timestamp'], how='left_outer').rdd
+gyroscopes = spark.read.csv("hdfs://192.168.0.104/user/automotive/gyroscopes.csv", header=True, mode="DROPMALFORMED")
+rdd = accelerations.join(gyroscopes, on=['trip_id', 'timestamp'], how='left_outer').rdd
 # positions = spark.read.csv("hdfs://192.168.0.104/user/automotive/positions.csv", header=True, mode="DROPMALFORMED")
 # rdd = joined.join(positions, on=['trip_id', 'timestamp'], how='left_outer').rdd
 # rdd = rdd.map(lambda x: (x[0], x[1:])).groupByKey().mapValues(list).collect()
-rdd.saveAsTextFile("hdfs://192.168.0.104/user/automotive/join.txt")
-#accelerations.saveAsHadoopFile("hdfs://192.168.0.104/user/automotive/a.txt", "org.apache.hadoop.mapred.TextOutputFormat")
+#rdd.saveAsTextFile("hdfs://192.168.0.104/user/automotive/join.txt")
+rdd.saveAsHadoopFile("hdfs://192.168.0.104/user/automotive/a.txt", "org.apache.hadoop.mapred.TextOutputFormat")
 spark.stop()
--- automotive/submit.sh
+++ automotive/submit.sh
@@ -20,7 +20,8 @@ export YARN_CONF_DIR="./etc"
 
 # spark-submit --conf spark.yarn.jars="hdfs://127.0.0.1:/spark/" \
 # ${SPARK_SUBMIT} --master spark://127.0.0.1:7077 \
-${SPARK_SUBMIT} --master spark://127.0.0.1:7077 \
+# ${SPARK_SUBMIT} --master spark://127.0.0.1:7077 \
+spark-submit --master spark://127.0.0.1:7077 \
 --deploy-mode client \
 --driver-memory 512m \
 --executor-memory 512m \
